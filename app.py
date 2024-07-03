357dd3d20cd96766b87fea198d4bfbcc47273494
michwdyn <90368653+michwdyn@users.noreply.github.com>
2024-07-03 11:22:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template, request
+import google.generativeai as palm
+import os
+
+palm.configure(api_key="AIzaSyBDZnlNh2tzP16nA1m0XsWVvY-vLt1d59Q")
+
+model = { 'model': "models/chat-bison-001"}
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    return(render_template("index.html"))
+    return render_template("index.html")
+
+@app.route("/main", methods=["GET","POST"])
+def main():
+    r = request.form.get("q")
+    print(r)
+    return(render_template("main.html",r=r))
+
+@app.route("/traffic_thailand", methods=["GET","POST"])
+def traffic_thailand():
+    q = "thailand traffic"
+    r = palm.chat(**model, messages=q)
+    return(render_template("traffic_thailand.html",r=r.last))
 
 if __name__ == "__main__":
     app.run()
